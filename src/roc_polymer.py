4e09e1e66ca7922c46ddbca1506d1299778f2955
graxia01 <y.graziadei@gmail.com>
2024-10-25 16:26:41 +0200

    roc funzionante

--- src/roc_polymer.py
+++ src/roc_polymer.py
@@ -13,6 +13,16 @@ from math import sqrt
 import embedding_polymer, detection_test_polymer
 
 
+# Predefined spirals used for embedding and extracting the watermark
+spiral1 = [(128, 128), (95, 98), (133, 191), (175, 66), (39, 143), (213, 182), (100, 21), (73, 234), (248, 85), (3, 77), (188, 257), (287, 163), (106, 301), (266, 12), (264, 263), (333, 101), (175, 339), (337, 224), (47, 353), (343, 15), (264, 339), (390, 149), (129, 399), (353, 298), (410, 51), (232, 409), (424, 219), (62, 437), (339, 376), (463, 111), (175, 466), (432, 300)]
+spiral2 = [(384, 128), (351, 98), (389, 191), (431, 66), (295, 143), (469, 182), (356, 21), (329, 234), (504, 85), (259, 77), (444, 257), (249, 206), (362, 301), (198, 121), (255, 283), (210, 7), (431, 339), (169, 196), (303, 353), (145, 65), (179, 287), (385, 399), (107, 153), (230, 373), (488, 409), (107, 259), (318, 437), (64, 89), (151, 364), (431, 466), (46, 205), (237, 452)]
+spiral3 = [(128, 384), (95, 354), (133, 447), (175, 322), (39, 399), (213, 438), (100, 277), (73, 490), (248, 341), (3, 333), (172, 241), (287, 419), (31, 246), (266, 268), (119, 187), (333, 357), (238, 192), (337, 480), (38, 168), (343, 271), (171, 132), (390, 405), (312, 181), (77, 103), (410, 307), (242, 103), (424, 475), (388, 199), (140, 55), (463, 367), (323, 101), (19, 48)]
+spiral4 = [(384, 384), (351, 354), (389, 447), (431, 322), (295, 399), (469, 438), (356, 277), (329, 490), (504, 341), (259, 333), (428, 241), (249, 462), (287, 246), (198, 377), (375, 187), (210, 263), (494, 192), (169, 452), (294, 168), (145, 321), (427, 132), (203, 188), (107, 409), (333, 103), (120, 249), (498, 103), (226, 114), (64, 345), (396, 55), (123, 169), (46, 461), (275, 48)]
+spiral5 = [(256, 256), (223, 226), (261, 319), (303, 194), (167, 271), (341, 310), (228, 149), (201, 362), (376, 213), (131, 205), (316, 385), (300, 113), (121, 334), (415, 291), (159, 118), (234, 429), (394, 140), (70, 249), (392, 391), (247, 59), (127, 411), (461, 229), (82, 135), (303, 467), (366, 64), (41, 324), (465, 352), (166, 40), (175, 481), (471, 143), (17, 193), (392, 467)]
+
+# List of spirals to be checked later for detection
+spirals = [spiral1, spiral2, spiral3, spiral4, spiral5]
+
 #this seed was set just to make you obtain the same result
 random.seed(3)
 def awgn(img, std, seed):
@@ -112,13 +122,13 @@ def compute_roc():
         original_image = cv2.imread(original_image, 0)
         
         #plot original and watermarked image
-        plt.subplot(1, 2, 1)
-        plt.imshow(original_image, cmap='gray')
-        plt.title('Original image')
-        plt.subplot(1, 2, 2)
-        plt.imshow(watermarked_image, cmap='gray')
-        plt.title('Watermarked image')
-        plt.show()
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(original_image, cmap='gray')
+        # plt.title('Original image')
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(watermarked_image, cmap='gray')
+        # plt.title('Watermarked image')
+        # plt.show()
 
         sample = 0
         while sample <= 9:
@@ -130,7 +140,23 @@ def compute_roc():
             attacked_image = random_attack(watermarked_image)
 
             # extract attacked watermark
-            wat_extracted_attacked = detection_test_polymer.detection(original_image, watermarked_image, attacked_image)
+            differences = detection_test_polymer.find_differences(original_image, watermarked_image)
+    
+            # Identify which spiral was used based on the differences
+            spiral_index = None
+            for idx, spiral in enumerate(spirals):
+                if detection_test_polymer.check_spiral_for_differences(differences, spiral):
+                    spiral_index = idx
+                    break
+
+            # If no matching spiral is found, raise an error
+            if spiral_index is None:
+                raise ValueError("No matching spiral found.")
+
+            print(f"[SPIRAL CENTER DETECTED]: {spirals[spiral_index][0]}")
+            
+            used_spiral = spirals[spiral_index]
+            wat_extracted_attacked = detection_test_polymer.extract_watermark(original_image, watermarked_image, used_spiral)
 
             # compute similarity H1
             scores.append(detection_test_polymer.similarity(watermark, wat_extracted_attacked))
